73a157c84d6d0b36cc7cd41e8cb81dd5c0b450a9
James Hager <james@avantiapplications.com>
2022-02-28 16:10:42 -0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+Project documents for my Devmountain Foundations capstone project.
+
+Planning documents are at https://github.com/johager/foundations-capstone-docs
+
+After cloning the repo, execute the following:
+
+npm init -y
+npm i express
+npm i bcryptjs
+npm i dotenv
+npm i sequelize
+
+
+The .env file should include
+PORT - port for the server
+CONNECTION_STRING - URI for the PostgreSQL database
---
+++ server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const path = require('path')
+
+const express = require('express')
+const app = express()
+
+const {PORT} = process.env
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(PORT, () => { console.log(`Up on port ${PORT}`) })
